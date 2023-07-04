fecb4624009d2be10ecfc9a2114126b2a5bc4dd3
Eamon Goldsmith <goldsmitheamon@gmail.com>
2023-07-05 09:32:17 +1000

    initial commit

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sockets.h>
+
+int main(int argc, char** argv)
+{
+	puts("Server starting...");
+
+	// handle commandline arguments
+	
+
+	// open socket
+	
+
+	// listen for new connections
+
+	return 0;
+}
